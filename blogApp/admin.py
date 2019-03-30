84520f6aff9a541047cf9c5309248ff3aea2de3e
DanyZy <daniilzgurovec@gmail.com>
2019-03-31 04:56:15 +0600

    added image to model

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
+
+class PostModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Post
+
+
 admin.site.register(Post)
--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -10,3 +12,7 @@ urlpatterns = [
     url(r'^registration/$', views.registration, name='registration'),
     url(r'^authorization/$', views.authorization, name='authorization'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
